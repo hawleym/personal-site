58e9ceedfde6e3c2f8a33b594f9eb2e9fe3f6c88
hawleym <hawley@arthena.com>
2021-01-21 10:46:01 -0500

    updates descriptions

--- components/nav.js
+++ components/nav.js
@@ -11,14 +11,14 @@ export default function Nav() {
       <ul className="flex items-center justify-between p-8">
         <li>
           <Link href="/">
-            <a className="text-blue-500 no-underline text-accent-1 dark:text-blue-300">
+            <a className="text-blue-500 no-underline text-accent-1 dark:text-blue-300 btn-blue">
               Home
             </a>
           </Link>
         </li>
         <li>
           <Link href="/about">
-            <a className="text-blue-500 no-underline text-accent-1 dark:text-blue-300">
+            <a className="text-blue-500 no-underline text-accent-1 dark:text-blue-300 btn-blue">
               About
             </a>
           </Link>
--- pages/index.js
+++ pages/index.js
@@ -5,9 +5,15 @@ export default function IndexPage() {
     <div>
       <Nav />
       <div className="py-20">
-        <h1 className="text-5xl text-center text-gray-700 dark:text-gray-100">
+        <h1 className="text-5xl text-center text-gray-900">
           Hawley Moore
         </h1>
+        <h2 className="text-2xl pt-5 text-center text-gray-900">
+          This is my website.
+        </h2>
+        <p className="text-l pt-5 text-center text-gray-900">
+          Hi, I'm Hawley. I work at Arthena.
+        </p>
       </div>
     </div>
   )
