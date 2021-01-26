61ef55e17d9144ca56631c7bb78ebcab87ee7e47
hawleym <hawley@arthena.com>
2021-01-26 17:50:39 -0500

    Updates

---
+++ _next/static/development/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST = {__rewrites:[],"/":["static\u002Fchunks\u002Fpages\u002Findex.js"],"/_error":["static\u002Fchunks\u002Fpages\u002F_error.js"],"/music":["static\u002Fchunks\u002Fpages\u002Fmusic.js"],sortedPages:["\u002F","\u002F_app","\u002F_error","\u002Fmusic"]};self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
