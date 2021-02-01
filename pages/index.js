d8853e874adc4996990a0aea00b1a9b4f787fc15
hawleym <hawley@arthena.com>
2021-02-01 11:35:25 -0500

    fixes spacing error

--- pages/index.js
+++ pages/index.js
@@ -5,7 +5,7 @@ export default function IndexPage() {
     <div>
       <Nav />
       <div className="pt-16">
-        <div className="pt-3">
+        <div className="sm:pt-3 pt-1">
           <img
             src="/images/art1.jpg"
             alt="Five works by Hawley Moore on a Wall"
